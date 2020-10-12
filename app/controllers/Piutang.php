6724887fdcc0b8cc52913a9d1df5c194b60e4c9f
FieryInferno <bagassetia271@gmail.com>
2020-10-12 13:08:59 +0700

    Perbaikan Template Piutang

--- app/controllers/Piutang.php
+++ app/controllers/Piutang.php
@@ -74,7 +74,7 @@ class Piutang extends User_Controller {
 		$data['subtitle'] = lang('list');
 		$data['content'] = 'Piutang/index';
 		$data = array_merge($data,path_info());
-		$this->parser->parse('default',$data);
+		$this->parser->parse('template',$data);
 	}
 
 	public function index_datatable() {
