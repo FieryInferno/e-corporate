2f05b10f54cf769e0c7d4344b2fd0aa697d8b5ec
FieryInferno <bagassetia271@gmail.com>
2020-10-24 16:56:58 +0700

    Perbaikan Faktur Pembelian

--- app/models/Faktur_pembelian_model.php
+++ app/models/Faktur_pembelian_model.php
@@ -66,7 +66,9 @@ class Faktur_pembelian_model extends CI_Model {
 			'kontakid'		    => $this->input->post('kontakid'),
 			'perusahaanid'	    => $this->input->post('perusahaanid'),
 			'total'			    => $total,
-			'bank'			    => $this->input->post('rekening')
+			'bank'			    => $this->input->post('rekening'),
+			'ppn'				=> $pajak,
+			'biayaPengiriman'	=> $biayapengiriman
 		]);
 		if($insert) {
 			$i  = 0;
