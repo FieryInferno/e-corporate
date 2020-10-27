74f2996394c351a991c5e74cc12ffef3ac77ece4
FieryInferno <bagassetia271@gmail.com>
2020-10-27 19:58:34 +0700

    Perbaikan Edit Anggaran Belanja

--- app/views/Anggaran_belanja/edit.php
+++ app/views/Anggaran_belanja/edit.php
@@ -27,7 +27,6 @@
                     <div class="card">
                         <div class="card-header">
                             <h3 class="card-title"><?= $title; ?> Anggaran Belanja</h3>
-                            <?php print_r($detail); ?>
                         </div>
                         <!-- /.card-header -->
                         <!-- form start -->
