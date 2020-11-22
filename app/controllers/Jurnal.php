508d0c25648a5faf0ed02e562f8715c09d8062b0
FieryInferno <bagassetia271@gmail.com>
2020-11-22 20:23:33 +0700

    Perbaikan Jurnal Umum Kas Bank

--- app/controllers/Jurnal.php
+++ app/controllers/Jurnal.php
@@ -378,32 +378,34 @@ class Jurnal extends User_Controller {
 						// $data1	= $this->db->get_where('tSetupJurnal')->result_array();
 						// $no		= 0;
 						// if ($data1) {
-						// 	$this->db->select('mnoakun.akunno, mnoakun.namaakun, mnoakun1.akunno as akunno1, mnoakun1.namaakun as namaakun1, mnoakun2.akunno as akunno2, mnoakun2.namaakun as namaakun2');
-						// 	$this->db->join('mnoakun', 'tPemetaanAkun.kodeAkun1 = mnoakun.idakun');
-						// 	$this->db->join('mnoakun as mnoakun1', 'tPemetaanAkun.kodeAkun2 = mnoakun1.idakun');
-						// 	$this->db->join('mnoakun as mnoakun2', 'tPemetaanAkun.kodeAkun3 = mnoakun2.idakun');
-						// 	$data2	= $this->db->get_where('tPemetaanAkun', [
-						// 		'kodeAkun'	=> $key['idakun']
-						// 	])->row_Array();
+							$this->db->select('mnoakun.akunno, mnoakun.namaakun, mnoakun1.akunno as akunno1, mnoakun1.namaakun as namaakun1, mnoakun2.akunno as akunno2, mnoakun2.namaakun as namaakun2');
+							$this->db->join('mnoakun', 'tPemetaanAkun.kodeAkun1 = mnoakun.idakun');
+							$this->db->join('mnoakun as mnoakun1', 'tPemetaanAkun.kodeAkun2 = mnoakun1.idakun');
+							$this->db->join('mnoakun as mnoakun2', 'tPemetaanAkun.kodeAkun3 = mnoakun2.idakun');
+							$data2	= $this->db->get_where('tPemetaanAkun', [
+								'kodeAkun'	=> $key['idakun']
+							])->row_Array();
 						// 	foreach ($data1 as $setupJurnal) {
-								$jenis	= ['debit', 'kredit'];
-								foreach ($jenis as $value) {
-									if ($key['penerimaan'] !== '0') {
-										$total	= $key['penerimaan'];
-									} else {
-										$total	= $key['pengeluaran'];
+								if ($data2) {
+									$jenis	= ['debit', 'kredit'];
+									foreach ($jenis as $value) {
+										if ($key['penerimaan'] !== '0') {
+											$total	= $key['penerimaan'];
+										} else {
+											$total	= $key['pengeluaran'];
+										}
+										array_push($data['jurnalUmum'], [
+											'tanggal'			=> $key['tanggal'],
+											'formulir'			=> 'Kas Bank',
+											'noTrans'			=> $key['nomor_kas_bank'],
+											'departemen'		=> '',
+											'nama_perusahaan' 	=> $key['nama_perusahaan'],
+											'akunno'			=> $key['akunno'],
+											'namaakun'			=> $key['namaakun'],
+											'jenis'				=> $value,
+											'total'				=> $total
+										]);
 									}
-									array_push($data['jurnalUmum'], [
-										'tanggal'			=> $key['tanggal'],
-										'formulir'			=> 'Kas Bank',
-										'noTrans'			=> $key['nomor_kas_bank'],
-										'departemen'		=> '',
-										'nama_perusahaan' 	=> $key['nama_perusahaan'],
-										'akunno'			=> $key['akunno'],
-										'namaakun'			=> $key['namaakun'],
-										'jenis'				=> $value,
-										'total'				=> $total
-									]);
 								}
 							// }
 						// }
